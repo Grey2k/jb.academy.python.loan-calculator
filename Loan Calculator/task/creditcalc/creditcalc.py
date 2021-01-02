cd4c49dfdb109b1cb3711599917246366d046bfb
Sergey Goppikov <grey2k@gmail.com>
2021-01-02 14:25:17 +0300

    Completer 2/4 stage

--- Loan Calculator/task/creditcalc/creditcalc.py
+++ Loan Calculator/task/creditcalc/creditcalc.py
@@ -1,7 +1,60 @@
-loan_principal = 'Loan principal: 1000'
-final_output = 'The loan has been repaid!'
-first_month = 'Month 1: repaid 250'
-second_month = 'Month 2: repaid 250'
-third_month = 'Month 3: repaid 500'
+import math
 
-# write your code here
+###
+interest = 0
+
+COMMAND_BY_PERIODS = 'p'
+COMMAND_BY_MONTHLY_PAYMENT = 'm'
+
+
+def welcome_command(command: str) -> None:
+    if command == COMMAND_BY_MONTHLY_PAYMENT:
+        print('Enter the monthly payment:')
+
+    if command == COMMAND_BY_PERIODS:
+        print('Enter the number of months:')
+
+
+def count_payments(months: int) -> None:
+    if principal % months == 0:
+        monthly = principal / months
+        print('Your monthly payment = {monthly}'.format(monthly=monthly))
+        return
+
+    monthly = math.ceil(principal / months)
+
+    last_month = principal - ((months - 1) * monthly)
+
+    print('Your monthly payment = {monthly} and the last payment = {last_month}.'.format(
+        monthly=monthly, last_month=last_month
+    ))
+
+
+def count_months(payment: int) -> None:
+    months = math.ceil(principal / payment)
+
+    print('It will take {months} {plural} to repay the loan'.format(
+        months=months, plural='months' if months > 1 else 'month'
+    ))
+
+
+def run_command(command, param) -> None:
+    if command == COMMAND_BY_MONTHLY_PAYMENT:
+        count_months(payment=param)
+
+    if command == COMMAND_BY_PERIODS:
+        count_payments(months=param)
+
+
+print('Enter the loan principal:')
+principal = int(input().strip())
+
+print('What do you want to calculate?')
+print('type "m" for number of monthly payments,')
+print('type "p" for the monthly payment')
+
+cmd = input().strip()
+
+welcome_command(cmd)
+
+run_command(cmd, int(input().strip()))
